81fc1a0a84c30e66d85368bca324b704d9bc5179
abhijeetmudholkar <126692571+abhijeetmudholkar@users.noreply.github.com>
2024-04-06 12:54:31 +0530

    photo not change problem solve

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -127,7 +127,7 @@ const Weather = () => {
   };
 
   return (
-    <div className={(typeof weather.mainc!="undefined") ? ((weather.main.temp>16)?'app-warm':'app'):'app'}>
+    <div className={(typeof weather.main!="undefined") ? ((weather.main.temp>16)?'app-warm':'app'):'app'}>
       <main>
         <div className="search-box">
           <input
