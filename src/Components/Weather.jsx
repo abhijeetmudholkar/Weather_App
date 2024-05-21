fbad2198ba76796af91ec21e80f2fe737d0dbc76
abhijeetmudholkar <126692571+abhijeetmudholkar@users.noreply.github.com>
2024-05-21 14:16:58 +0530

    update new ui

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,88 +1,5 @@
-// import { useState } from "react";
-
-// const Weather = () => {
-//   const [query, setQuery] = useState("");
-
-//   const [weather, setWeather] = useState({});
-
-//   const api = {
-//     key: "bbf8f01a167651a276d3de876de8172b",
-//     base: "https://api.openweathermap.org/data/2.5/",
-//   };
-
-//   const search = (evt) => {
-//     if (evt.key === "Enter") {
-//       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-//         .then((res) => res.json()) // Corrected invocation of res.json()
-//         .then((result) => {
-//           setWeather(result);
-//           setQuery("");
-//           console.log(weather);
-//         });
-//     }
-//   };
-
-//   const dateBuilder = (d) => {
-//     let months = [
-//       "January",
-//       "February",
-//       "March",
-//       "April",
-//       "May",
-//       "June",
-//       "July",
-//       "August",
-//       "September",
-//       "October",
-//       "November",
-//       "December",
-//     ];
-//     let days = [
-//       "Sunday",
-//       "Monday",
-//       "Tuesday",
-//       "Wednesday",
-//       "Thursday",
-//       "Friday",
-//       "Saturday",
-//     ];
-//     let day = days[d.getDay()];
-//     let date = d.getDate();
-//     let month = months[d.getMonth()];
-//     let year = d.getFullYear();
-
-//     return `${day} ${date} ${year}`;
-//   };
-//   return (
-//     <div>
-//       <main>
-//         <div className="search-box">
-//           <input
-//             type="text"
-//             className="search-bar"
-//             placeholder="Search..."
-//             onChange={(e) => setQuery(e.target.value)}
-//             onKeyPress={search}
-//           />
-//         </div>
-//         <div>
-//           <div className="location-box">
-//             <div className="location">
-//               {weather.name},{weather.sys.country}
-//               <div className="date">{dateBuilder(new Date())}</div>
-//             </div>
-//           </div>
-//         </div>
-//       </main>
-//     </div>
-//   );
-// };
-
-// export default Weather;
-
-
-import  { useState, useEffect } from "react";
-import './Weather.css'
+import { useState, useEffect } from "react";
+import './Weather.css';
 
 const Weather = () => {
   const [query, setQuery] = useState("");
@@ -123,7 +40,7 @@ const Weather = () => {
     const month = months[d.getMonth()];
     const year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`;
+    return `${day}, ${date} ${month} ${year}`;
   };
 
   return (
@@ -144,19 +61,21 @@ const Weather = () => {
           />
         </div>
         {weather.name && (
-        <div className="location-box">
+        <div className="weather-container">
+          <div className="location-box">
             <div className="location">
               {weather.name}, {weather.sys.country}
-              <div className="date">{dateBuilder(new Date())}</div>
             </div>
-            <div className="weather-box">
-                <div className="temp">
-                    {Math.round(weather.main.temp)}°C
-                </div>
-                <div className="weather">
-                    {weather.weather[0].main}
-                </div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">{Math.round(weather.main.temp)}°C</div>
+            <div className="weather">{weather.weather[0].description}</div>
+            <div className="details">
+              <div className="detail">Humidity: {weather.main.humidity}%</div>
+              <div className="detail">Wind Speed: {weather.wind.speed} m/s</div>
             </div>
+          </div>
         </div>
         )}
       </main>
